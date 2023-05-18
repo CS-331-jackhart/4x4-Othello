6fc8c351c5f011374f41a7ec1634882818468c12
jackhart591 <jackhart591@gmail.com>
2023-05-17 18:55:33 -0700

    Collected data

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-echo "Testing search vs. depth\n\n"
-echo "depth;pruning;heuristic;nodes\n"
+echo -e "Testing search vs. depth\n"
+echo "depth;pruning;heuristic;nodes"
 for I in 2 4 6 8 10 12 # Depth
 do
   for J in 0 1 # Pruning
@@ -8,13 +8,13 @@ do
     for K in 0 1 2 # Heuristic
     do
       NODES=$(python GameDriver.py alphabeta alphabeta $K $J $K $J $I $I 0)
-      echo "$I;$J;$K;$NODES\n"
+      echo "$I;$J;$K;$NODES"
     done
   done
 done
 
-echo "Testing heuristic quality\n\n"
-echo "depth;h1;h2;win\n"
+echo -e "Testing heuristic quality\n\n"
+echo -e "depth;h1;h2;win\n"
 for I in 2 4 6 8 # Depth
 do
   for J in 0 1 2 # Heuristic
@@ -23,7 +23,7 @@ do
     do
       if [[ $J -ne $K ]]; then
         WIN=$(python GameDriver.py alphabeta alphabeta $J 1 $K 1 $I $I 1)
-        echo "$I;$J;$K;$WIN\n"
+        echo "$I;$J;$K;$WIN"
       fi
     done
   done
